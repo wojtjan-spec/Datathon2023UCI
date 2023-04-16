e0957fdd22e964b6c80acc866bc095b0dd9c88f6
Jan Wojtkowski <82758491+wojtjan-spec@users.noreply.github.com>
2023-04-15 17:55:05 -0700

    Added new files after web makeover
    1. Changed structure of our directory
    2. Added css file with new styles
    3. Added fonts
    4. Added new functions to the app.py

--- app.py
+++ app.py
@@ -1,23 +1,43 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, url_for, flash
 import pickle
+import os
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+app = Flask(__name__, static_url_path='/static')
+app.config["IMAGE_UPLOAD"] = "/Users/janwojtkowski/Desktop/Datathon/imgs"
+#model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     #mood = int(request.form["1"])
+#     #song = int(request.form["2"])
+#     #prediction = model.predict([[1, 2]])
+#     #output = round(prediction[0], 2) 
+#     img = float(request.form["image"])
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    """Grabs the input values and uses them to make prediction"""
-    rooms = int(request.form["rooms"])
-    distance = int(request.form["distance"])
-    prediction = model.predict([[rooms, distance]])  # this returns a list e.g. [127.20488798], so pick first element [0]
-    output = round(prediction[0], 2) 
+#     return render_template('index.html', prßediction_text=f'From placeholder mood -> song: placeholder')
 
-    return render_template('index.html', prediction_text=f'A house with {rooms} rooms and located {distance} meters from the city center has a value of ${output}')
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files['image']
+            image.save(os.path.join(app.config['IMAGE_UPLOAD'], image.filename))
+            return render_template("index.html", uploaded_image=image.filename)
+        return render_template("index.html")
+
+@app.route('/uploads/<filename>')
+def send_uploaded_file(filename=''):
+    from flask import send_from_directory
+    return send_from_directory(app.config["IMAGE_UPLOAD"], filename)
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
